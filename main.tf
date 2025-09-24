022310eb494561ecf2b1c7f57fc99085e0ceb81f
surendar <surendarsit@gmail.com>
2025-09-24 13:06:32 +0530

    cidr block updated and env also changed

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc" "user26vpc54" {
-    cidr_block = "10.26.0.0/16"
+    cidr_block = "10.226.0.0/16"
     tags = {
       Name = "tcsuser26vpc"
-      env = "usertest26vpc"
+      env = "NON-PROD"
     }
   
 }
